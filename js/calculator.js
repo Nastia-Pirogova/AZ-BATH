d82ef6597bf8bb4c930e7ce003703a7b8be41781
Anastasia Pirogova <120117437+pirogovairina12@users.noreply.github.com>
2023-05-19 17:37:02 +0300

    update page

--- js/calculator.js
+++ js/calculator.js
@@ -1,14 +1,38 @@
-const minus = document.querySelector(".minus-btn");
-const plus = document.querySelector(".plus-btn");
-const quantity = document.querySelector(".js-quantity");
+// const minus = document.querySelectorAll(".minus-btn");
+// const plus = document.querySelectorAll(".plus-btn");
+// const quantity = document.querySelectorAll(".js-quantity");
+//
+// plus.addEventListener("click", () => {
+//     quantity.value = Number.parseInt(quantity.value) + 1
+// });
+// minus.addEventListener("click", () => {
+//     if (quantity.value > 1) {
+//         quantity.value = quantity.value - 1;
+//     }
+// });
 
-plus.addEventListener("click", () => {
-    quantity.value = Number.parseInt(quantity.value) + 1
+
+const minusBtns = document.querySelectorAll(".minus-btn");
+const plusBtns = document.querySelectorAll(".plus-btn");
+const quantities = document.querySelectorAll(".js-quantity");
+
+minusBtns.forEach((minusBtn, index) => {
+    minusBtn.addEventListener("click", () => {
+        if (quantities[index].value > 1) {
+            quantities[index].value = Number.parseInt(quantities[index].value) - 1;
+        }
+    });
 });
-minus.addEventListener("click", () => {
-    if (quantity.value > 1) {
-        quantity.value = quantity.value - 1;
-    }
+
+plusBtns.forEach((plusBtn, index) => {
+    plusBtn.addEventListener("click", () => {
+        quantities[index].value = Number.parseInt(quantities[index].value) + 1;
+    });
 });
+
+const buttonUpdate = document.querySelector(".wp-element-button");
+
+buttonUpdate.disabled = false;
+buttonUpdate.removeAttribute("aria-disabled");
 
 
